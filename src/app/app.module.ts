4c9039188dd1f40c6489784a05c5e1b2fe52b94e
MattE55 <89932886+MattE55@users.noreply.github.com>
2022-05-31 23:03:46 -0700

    Lowering size
    trying to lower the size again for a ghpages upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,15 +21,15 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MatIconModule } from '@angular/material/icon';
 import { DirectorCardComponent } from './director-card/director-card.component';
 import { GenreCardComponent } from './genre-card/genre-card.component';
-import { ProfileViewComponent } from './profile-view/profile-view.component';
-import { SynopsisCardComponent } from './synopsis-card/synopsis-card.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { NavbarComponent } from './navbar/navbar.component';
+//import { ProfileViewComponent } from './profile-view/profile-view.component';
+//import { SynopsisCardComponent } from './synopsis-card/synopsis-card.component';
+//import { EditProfileComponent } from './edit-profile/edit-profile.component';
+//import { NavbarComponent } from './navbar/navbar.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
-  { path: 'profile', component: ProfileViewComponent },
+  //{ path: 'profile', component: ProfileViewComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
@@ -42,10 +42,10 @@ const appRoutes: Routes = [
     WelcomePageComponent,
     DirectorCardComponent,
     GenreCardComponent,
-    ProfileViewComponent,
-    SynopsisCardComponent,
-    EditProfileComponent,
-    NavbarComponent
+    //ProfileViewComponent,
+    //SynopsisCardComponent,
+    //EditProfileComponent,
+    //NavbarComponent
   ],
   imports: [
     BrowserModule,
